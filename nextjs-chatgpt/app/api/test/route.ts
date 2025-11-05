7ca4e9a74cfaa81fc8f57ed79a040cd01b62225c
nk0o0o <94949582+nk0o0o@users.noreply.github.com>
2025-11-05 21:36:36 +0900

    Error Page / Not Found Page

--- nextjs-chatgpt/app/api/test/route.ts
+++ nextjs-chatgpt/app/api/test/route.ts
@@ -6,6 +6,8 @@ export async function GET(request: NextRequest) {
   const randomDelay = Math.floor(Math.random()*5000);
   console.log("randomDelay",randomDelay);
 
+  throw new Error();
+
   await delay(randomDelay);
 
   return NextResponse.json({data: `${randomDelay}만에 응답완료!`})
